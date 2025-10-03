56c2d2f2b952e42e97a47624e28390d4f0359ee9
magic_rb <richard@brezak.sk>
2025-10-03 15:56:15 +0200

    Drop renovate Nix flake patch
    Signed-off-by: magic_rb <richard@brezak.sk>

--- nixos/modules/buildbot_renovation.nix
+++ nixos/modules/buildbot_renovation.nix
@@ -10,28 +10,7 @@ in
       default = ps: ps.callPackage ../../package.nix {};
     };
 
-    renovate.package = lib.mkOption {
-      type = lib.types.package;
-      default =
-        if cfg.renovate.nixPatch
-        then pkgs.renovate.overrideAttrs (
-          final: _prev:
-          {
-            src = pkgs.fetchFromGitHub {
-              owner = "renovatebot";
-              repo = "renovate";
-              rev = "499c353780dce0df80fb44f84918c512a00d4ba1";
-              hash = "sha256-ePNar0asjwfMvNVz/N32hYneim13/Q1jOLXFbyZaDkY=";
-            };
-
-            pnpmDeps = pkgs.pnpm_10.fetchDeps {
-              inherit (final) pname version src;
-              hash = "sha256-tf7WO/i9pFW+FNNqqPGuZE5mkJ841OgM3sqIZ8ZnU0g=";
-            };
-          }
-        )
-        else pkgs.renovate;
-    };
+    renovate.package = lib.mkPackageOption pkgs "renovate" {};
 
     renovate.nixPatch = lib.mkEnableOption "Include Nix support patch in renovate";
   };
